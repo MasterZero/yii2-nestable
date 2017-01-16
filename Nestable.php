25d451e7de2d6687f704f247cf1725b1df61698a
MasterZero <sergey.sv5@mail.ru>
2017-01-16 21:46:33 +0600

    create event, for upload new set struct

--- Nestable.php
+++ Nestable.php
@@ -13,27 +13,21 @@ use yii\helpers\Url;
 
 class Nestable extends \slatiusa\nestable\Nestable
 {
-    public $autoQuery = null;
-    public $rootable = true;
 
     public $modelOptions = null;
 
     public $columns = ['name' => 'name'];
     public $buttons = null;
-
-    public $hideButtons = false;
-    
     public $driveController = '';
-    
-    public function init(){
-        $this->registerTranslations();
+    public $pluginEvents = null;
 
-        if(!is_null($this->autoQuery)){
-            $auto = $this->autoQuery->roots();
-            if($this->rootable)
-                $this->query = $auto;
-            else
-                $this->query = $auto->one()?$auto->one()->children(1):null;
+    public function init()
+    {
+
+
+        if(!is_null($this->query)){
+            $auto = $this->query->roots();
+            $this->query = $auto;
         }
 
         if(is_null($this->modelOptions)){
@@ -41,68 +35,64 @@ class Nestable extends \slatiusa\nestable\Nestable
         }
 
 
-        if(count($this->columns)==1&&!$this->hideButtons){
-            $this->columns['url'] = function($data){
+        if(is_null($this->pluginEvents)){
+            $this->pluginEvents = [ 'change' =>
+                    'function(e)
+                    {
+                        data = $(this).nestable("serialize");
+                        $.ajax({
+                            type: "GET",
+                            url: "'.Url::to(['restruct']).'",
+                            data: "data="+JSON.stringify(data),
+                            success: function(data) {
+                                if(!data)
+                                    console.error( "unable to restuct this sets" );
+                                },
+                            error: function(data) {
+                                console.error( "unable to restuct this sets" );
+                            },
+                            contentType : \'application/json\',
+                        });
+                    }',
+                ];
+        }
+
+
+
+         $this->columns['url'] = function($data){
                 return Url::toRoute([$this->driveController.'update', 'id' => $data->primaryKey]);
             };
-        }
 
         if(is_null($this->buttons)){
             $model = new $this->query->modelClass;
             $this->buttons = [
+
                 ['label' => Icon::show('pencil', [], Icon::FA),
                     'url' => function($data){ return Url::toRoute([$this->driveController.'update', 'id'=>$data->primaryKey]);},
-                    'options'=>['title'=>self::t('messages', 'Edit'), 'data-pjax' => 0]],
-                /*['label' => Icon::show('copy', [], Icon::FA),
-                    'url' => function($data){ return Url::toRoute([$this->driveController.'duplicate', 'id'=>$data->primaryKey]);},
-                    'options'=>['title'=>self::t('messages', 'Copy')],
-                    'visible' => $model->hasMethod('duplicate')],*/
-                ['label' => Icon::show('lock', [], Icon::FA),
-                    'url' => function($data){ return Url::toRoute([$this->driveController.'lock', 'id'=>$data->primaryKey]);},
+                    'options'=>['title'=>'Редактировать', 'data-pjax' => 0]],
+
+                ['label' => Icon::show('eye', [], Icon::FA),
+                    'url' => function($data){ return Url::toRoute([$this->driveController.'hide', 'id'=>$data->primaryKey]);},
                     'options'=>[
-                        'title'=>self::t('messages', 'Lock'),
+                        'title'=>'Скрыть',
                         'data-method' => 'POST',
                         'data-pjax' => '0',
-                    ],
-                    'visible' => function($data){ return $data->hasAttribute('locked')&&!$data->locked;}],
-                ['label' => Icon::show('unlock', [], Icon::FA),
-                    'url' => function($data){ return Url::toRoute([$this->driveController.'unlock', 'id'=>$data->primaryKey]);},
-                    'options'=>[
-                        'title'=>self::t('messages', 'Unlock'),
-                        'data-method' => 'POST',
-                        'data-pjax' => '0',
-                    ],
-                    'visible' => function($data){ return $data->hasAttribute('locked')&&$data->locked;}],
-                ['label' => Icon::show('trash', [], Icon::FA),
-                    'url' => function($data){ return Url::toRoute([$this->driveController.'delete', 'id'=>$data->primaryKey]);},
-                    'options'=>[
-                        'title'=>self::t('messages', 'To trash'),
-                        'data-method' => 'POST',
-                        'data-pjax' => '0',
-                        'data-confirm'=>"Вы действительно хотите удалить этот элемент?",
+                        'data-confirm'=>"Вы действительно хотите скрыть этот элемент?",
                     ],
                     'visible' => function($data){ return $data->hasAttribute('removed')&&!$data->removed;}],
-                ['label' => Icon::show('share-square-o', [], Icon::FA),
-                    'url' => function($data){ return Url::toRoute([$this->driveController.'restore', 'id'=>$data->primaryKey]);},
-                    'options'=>['title'=>self::t('messages', 'Restore')],
+                ['label' => Icon::show('eye-slash', [], Icon::FA),
+                    'url' => function($data){ return Url::toRoute([$this->driveController.'hide', 'id'=>$data->primaryKey]);},
+                    'options'=>['title'=>'Восстановить'],
                     'visible' => function($data){ return $data->hasAttribute('removed')&&$data->removed;}],
                 ['label' => Icon::show('remove', [], Icon::FA),
                     'url' => function($data){ return Url::toRoute([$this->driveController.'delete', 'id'=>$data->primaryKey]);},
                     'options'=>[
-                        'title'=>self::t('messages', 'Delete'),
+                        'title'=>'Удалить',
                         'data-method' => 'POST',
                         'data-pjax' => '0',
                         'data-confirm'=>"Вы действительно хотите удалить этот элемент?",
                     ],
-                    'visible' => function($data){
-                        if($data->hasAttribute('removed')){
-                            if(is_bool($data->removed))
-                                return $data->removed;
-
-                            return !is_null($data->removed);
-                        }
-                        return true;
-                    }],
+                    ],
             ];
         }
 
@@ -111,23 +101,6 @@ class Nestable extends \slatiusa\nestable\Nestable
         parent::init();
     }
 
-    public function registerTranslations()
-    {
-        $i18n = \Yii::$app->i18n;
-        $i18n->translations['masterzero/widgets/nestable/*'] = [
-            'class' => 'yii\i18n\PhpMessageSource',
-            'sourceLanguage' => 'en-US',
-            'basePath' => '@vendor/masterzero/yii2-nestable/messages',
-            'fileMap' => [
-                'masterzero/widgets/nestable/messages' => 'messages.php',
-            ],
-        ];
-    }
-
-    public static function t($category, $message, $params = [], $language = null)
-    {
-        return Yii::t('masterzero/widgets/nestable/' . $category, $message, $params, $language);
-    }
 
     /**
      * Register client assets
@@ -160,7 +133,7 @@ class Nestable extends \slatiusa\nestable\Nestable
             }
         }
 
-        if(!is_null($this->buttons)&&!$this->hideButtons){
+        if(!is_null($this->buttons)){
             $template = '<div class="pull-right" style="margin-top: -2px;">{buttons}</div>';
             $myButtons = $this->buttons;
             foreach($myButtons as $key => &$button){
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # yii2-nestable
-fixed version of sibds creator's
+Plugin for [yii2-nestable by ASlatius](https://github.com/ASlatius/yii2-nestable) to view nested sets nodes
+
+Installation
+------------
+
+Before install you need have [composer](http://getcomposer.org/download/).
+
+
+Make sure you've attached the [NestedSetsBehavior by creocoder](https://github.com/creocoder/yii2-nested-sets) correctly to your model.
+Then add the node move handler to you controller by attaching the supplied action;
+
+Either run
+
+```
+$ php composer.phar require masterzero/yii2-nestable "*"
+```
+
+or add
+
+```
+"masterzero/yii2-nestable": "*"
+```
+
+to the ```require``` section of your `composer.json` file.
+
+## Usage
+
+
+```
+<?= masterzero\widgets\Nestable::widget([
+            'query' => \common\models\NestedSetModel::find(),
+        ]);
+?>
+```
